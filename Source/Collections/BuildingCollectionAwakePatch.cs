3e892fd737aa641f68f4668ff9c4f89540db2045
Grimmys <funkyzermmy@gmail.com>
2022-06-15 13:12:11 +0200

    Add Snowdump to loaded assets

--- Source/Collections/BuildingCollectionAwakePatch.cs
+++ Source/Collections/BuildingCollectionAwakePatch.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System.Linq;
+using HarmonyLib;
 
 namespace SnowfallAssetsEverywhere
 {
@@ -10,10 +11,15 @@ namespace SnowfallAssetsEverywhere
         {
             if (Utils.ShouldBeSkipped(__instance))
             {
-                if (__instance.gameObject?.name != Constants.WINTER_BEAUTIFICATION && __instance.gameObject?.name != Constants.WINTER_MONUMENT)
+                if (__instance.gameObject?.name != Constants.WINTER_BEAUTIFICATION && __instance.gameObject?.name != Constants.WINTER_MONUMENT && __instance.gameObject?.name != Constants.WINTER_GARBAGE)
                 {
                     UnityEngine.Object.Destroy(__instance);
                     return false;
+                }
+                if (__instance.gameObject?.name == Constants.WINTER_GARBAGE)
+                {
+                    __instance.m_prefabs = __instance.m_prefabs.Where(prefab => prefab.name == "Snowdump").ToArray();
+                    __instance.m_replacedNames = null;
                 }
             }
             return true;
--- Source/Constants.cs
+++ Source/Constants.cs
@@ -6,6 +6,7 @@
         public const string SNOWFALL_LEVEL_SCENE = "WinterPrefabs";
         public const string WINTER_BEAUTIFICATION = "Winter Beautification";
         public const string WINTER_MONUMENT = "Winter Monument";
+        public const string WINTER_GARBAGE = "Winter Garbage";
         public const string WINTER_COLLECTIONS = "Winter Collections";
         public const string WINTER_EXPANSION_1 = "Winter Expansion 1";
         public const string WINTER_PREORDER_PACK = "Winter Preorder Pack";
