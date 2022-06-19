192516ae43474df271e9bd321c55349407891f39
Grimmys <funkyzermmy@gmail.com>
2022-06-19 17:32:40 +0200

    Don't load all scenes at once, wait for the previous one to be finished before starting a new one

--- Source/SceneManagerLoadSceneAsyncPatch.cs
+++ Source/SceneManagerLoadSceneAsyncPatch.cs
@@ -1,24 +1,41 @@
 ﻿using UnityEngine.SceneManagement;
 using HarmonyLib;
 using System;
+using UnityEngine;
+using System.Collections.Generic;
 
 namespace SnowfallAssetsEverywhere
 {
+
     [HarmonyPatch(typeof(SceneManager), "LoadSceneAsync", new Type[] { typeof(string), typeof(LoadSceneMode) })]
     public static class SceneManagerLoadSceneAsyncPatch
     {
+        public static AsyncOperation currentWinterOperation = null;
+        public static Queue<string> nextWinterLevels = new Queue<string>();
+
+
         public static void Prefix(SceneManager __instance, string sceneName, LoadSceneMode mode)
         {
+            if (currentWinterOperation != null && currentWinterOperation.isDone)
+            {
+                currentWinterOperation = null;
+                if (nextWinterLevels.Count > 0)
+                {
+                    currentWinterOperation = SceneManager.LoadSceneAsync(nextWinterLevels.Dequeue(), LoadSceneMode.Additive);
+                }
+
+            }
+
             if (Utils.IsSnowfallInstalled())
             {
                 if (sceneName == Utils.GetNativeLevelScene() && Utils.GetNativeLevelScene() != Constants.SNOWFALL_LEVEL_SCENE)
                 {
-                    SceneManager.LoadSceneAsync(Constants.SNOWFALL_LEVEL_SCENE, mode);
+                    currentWinterOperation = SceneManager.LoadSceneAsync(Constants.SNOWFALL_LEVEL_SCENE, mode);
                     if (Utils.IsAfterDarkInstalled())
                     {
-                        SceneManager.LoadSceneAsync(Constants.SNOWFALL_AFTERDARK_SCENE, LoadSceneMode.Additive);
+                        nextWinterLevels.Enqueue(Constants.SNOWFALL_AFTERDARK_SCENE);
                     }
-                    SceneManager.LoadSceneAsync(Constants.SNOWFALL_SIGNUP_PACK_SCENE, LoadSceneMode.Additive);
+                    nextWinterLevels.Enqueue(Constants.SNOWFALL_SIGNUP_PACK_SCENE);
                 }
             }
         }
